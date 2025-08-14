52aa9d34fce8f2ed725e3fbcbd4e583533cf1cc6
azeezabidoye <azeezabidoye@yahoo.com>
2025-08-14 17:03:12 +0100

    Script updated

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -4,4 +4,10 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";
 
-contract DeploySimpleStorage is Script {}
+contract DeploySimpleStorage is Script {
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcast();
+        // Code to be deployed
+        vm.stopBroadcast();
+    }
+}
